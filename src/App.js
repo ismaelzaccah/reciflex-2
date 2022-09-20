791c348eb729de72e35fbd75bd6f29d6bb7b27de
Ismael Zaccah <ismael.zaccah@gmail.com>
2022-09-20 18:07:40 -0300

    start creating initial layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import { Box, Paper, TextField, CssBaseline, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="App">
+      <CssBaseline>
+        <Box
+          component="form"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            flexWrap: 'wrap',
+            '& > :not(style)': {
+              m: 1,
+              width: 800,
+              height: 600,
+            },
+          }}>
+          <Paper elevation={8}>
+
+            <Box sx={{ display: 'flex' }}>
+              <Box sx={{ display: 'flex' }}>
+                <img src={logo} alt="Reciflex 2 logo" width={200} />
+                <Typography variant='subtitle1'>2.0</Typography>
+              </Box>
+
+              <Typography>Desenvolvido por Ismael Zaccah</Typography>
+            </Box>
+
+            <TextField fullWidth />
+          </Paper>
+        </Box>
+      </CssBaseline>
+
+    </Box>
+
   );
 }
 
