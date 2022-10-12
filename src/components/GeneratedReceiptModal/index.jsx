2872b0b76d8a872a11db88633bb56e7f09945455
Ismael Zaccah - PopOS <ismael.zaccah@gmail.com>
2022-10-11 22:17:28 -0300

    start create modal print

--- src/components/GeneratedReceiptModal/index.jsx
+++ src/components/GeneratedReceiptModal/index.jsx
@@ -1,11 +1,14 @@
 import { Box, Typography } from "@mui/material"
+import { Stack } from "@mui/system";
+import companyLogo from "../../assets/logo.png"
+import company from "../../config";
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  // width: 400,
   bgcolor: 'background.paper',
   boxShadow: 24,
   p: 4,
@@ -22,11 +25,36 @@ const GeneratedReceiptModal = () => { //FIXME - Insert props
     signature: "3",
   }
 
+  company.companyName = ""; //FIXME - remove after tests
+
   return (
     <Box sx={style}>
-      <Typography>
-        {props.clientName}
-      </Typography>
+      <Stack
+        spacing={2}
+        direction='row'
+        justifyContent='space-around'
+      >
+        <img
+          src={companyLogo}
+          alt={company.companyName}
+          width={200}
+          draggable={false}
+        />
+        <Stack
+          spacing={2}
+          direction='row'
+          justifyContent='space-around'
+        >
+          <Typography>
+            {company.companyName.toUpperCase() || "Nome da empresa não configurado"}
+          </Typography>
+          <Typography>
+            {props.includeCNPJ && (company.cnpj || "CNPJ não configurado")}
+            <br />
+          </Typography>
+
+        </Stack>
+      </Stack>
     </Box>
   )
 }
