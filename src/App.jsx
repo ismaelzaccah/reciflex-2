ae3f5e4ec91540cdaaed241f1ad708a662ba1c00
Ismael Zaccah <ismael.zaccah@gmail.com>
2022-10-18 10:25:01 -0300

    fix font family

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import './App.css';
 import * as React from 'react';
 import { CssBaseline } from '@mui/material';
 import Home from './pages/Home'
+
 
 const App = () => {
   return (
